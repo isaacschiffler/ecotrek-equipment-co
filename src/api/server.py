76d13175984ce48d9a5b6014074b63d03c3d8c76
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-05 12:02:12 -0700

    removed bad router import

--- src/api/server.py
+++ src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import carts, catalog, bottler, barrels, admin, info, inventory
+from src.api import admin
 import json
 import logging
 import sys
