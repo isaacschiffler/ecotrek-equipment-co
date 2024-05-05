5f5aae764a070a3509854533414bef40248d4838
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-05 11:42:49 -0700

    server.py

---
+++ src/api/server.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, exceptions
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from src.api import carts, catalog, bottler, barrels, admin, info, inventory
+import json
+import logging
+import sys
+from starlette.middleware.cors import CORSMiddleware
+import sqlalchemy
+from src import database as db
+
+
+description = """
+Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+"""
+
+app = FastAPI(
+    title="eco-trek-equipment-co.",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Isaac Schiffler",
+        "email": "ischiffl@calpoly.edu",
+    },
+)
+
+origins = ["https://potion-exchange.vercel.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+app.include_router(inventory.router)
+app.include_router(carts.router)
+app.include_router(catalog.router)
+app.include_router(bottler.router)
+app.include_router(barrels.router)
+app.include_router(admin.router)
+app.include_router(info.router)
+
+@app.exception_handler(exceptions.RequestValidationError)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request, exc):
+    logging.error(f"The client sent invalid data!: {exc}")
+    exc_json = json.loads(exc.json())
+    response = {"message": [], "data": None}
+    for error in exc_json:
+        response['message'].append(f"{error['loc']}: {error['msg']}")
+
+    return JSONResponse(response, status_code=422)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Eco-Trek Equipment Co."}
