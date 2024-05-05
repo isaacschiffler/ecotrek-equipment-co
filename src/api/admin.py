74ce2c180e007bb621f9a7ef71377b7f24911373
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-05 11:22:02 -0700

    src and test endpoint

---
+++ src/api/admin.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
+import sqlalchemy
+from src import database as db
+
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+@router.post("/reset")
+def reset():
+    """
+    TEST....
+    """
+
+
+                 
+    return "OK"
+
---
+++ src/database.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+from sqlalchemy import create_engine
+import sqlalchemy
+
+def database_connection_url():
+    dotenv.load_dotenv()
+
+    return os.environ.get("POSTGRES_URI")
+
+engine = create_engine(database_connection_url())
+
+# create tables in metadata
+metadata_obj = sqlalchemy.MetaData()
