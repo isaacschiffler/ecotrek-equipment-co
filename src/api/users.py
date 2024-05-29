068cd7fabdf540c32307b9c48eac2d3b1a2e4c9e
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-05-28 23:01:13 -0700

    code review feedback, updates to user registration  pref. activities

--- src/api/users.py
+++ src/api/users.py
@@ -45,22 +45,26 @@ def user_register(newUser: User):
     """
 
     with db.engine.begin() as connection:
+        # converting enum to str when add to table
+        preferred_activities_str = ','.join(activity.value for activity in newUser.preferred_activities)
+
         email_check = text("SELECT id FROM users WHERE email = :email")
         phone_check = text("SELECT id FROM users WHERE phone_number = :phone_number ")
         existing_email = connection.execute(email_check, {"email": newUser.email}).scalar()
         existing_phone = connection.execute(phone_check, {"phone_number": newUser.phone_number}).scalar()
 
+
         if existing_email or existing_phone:
-            # If the email exists, return a message indicating the email is already in use
+            # email or phone number already exist in table, avoiding duplicate rows
             return {"userID": None, "success": False, "message": "Email and/or Phone Number already exists"}
 
         userID = connection.execute(
-            sqlalchemy.text("INSERT INTO users (name, email, phone_number, preferred_activites) "
+            sqlalchemy.text("INSERT INTO users (name, email, phone_number, preferred_activities) "
                             "VALUES (:name, :email, :phone_number, :preferred_activities) RETURNING id"),
                             {"name": newUser.name,
                              "email": newUser.email,
                             "phone_number": newUser.phone_number, 
-                            "preferred_activities": newUser.preferred_activities}).scalar_one()
+                            "preferred_activities": preferred_activities_str}).scalar_one()
     
         print(f"Creating user for {newUser.name} with id {userID}")
 
