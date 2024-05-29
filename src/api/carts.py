6f2e511991ec44ba3073fe59615e8eff72a27ed5
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-05-28 23:12:01 -0700

    code review feedback, carts cleanup

--- src/api/carts.py
+++ src/api/carts.py
@@ -49,18 +49,28 @@ class CartItem(BaseModel):
 
 @router.post("/{cart_id}/items/{productID}")
 def set_item_quantity(cart_id: int, product_id: int, cart_item: CartItem):
-    """ add quantity of items """
+    """ add quantity of items 
+    REQ
+    {
+    "quantity": "integer"
+    }
+    RES
+    {
+    "success": "boolean"
+    }
+
+    """
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("""INSERT INTO cart_items (cart_id, product_id, quantity, price)
                                            SELECT :cart_id, :product_id, :quantity, products.sale_price
                                            FROM products
                                            WHERE products.id = :product_id
                                            """),
-                                           [{
+                                           {
                                                'cart_id': cart_id,
                                                'quantity': cart_item.quantity,
                                                'product_id': product_id
-                                           }])
+                                           })
         
     print("new entry in cart " + str(cart_id) + ". Product id: " + str(product_id))
 
@@ -72,7 +82,17 @@ class CartCheckout(BaseModel):
 
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
-    """Subtract item quantity and add money"""
+    """Subtract item quantity and add money
+    REQ
+    {
+    "payment": "string"
+    }
+    RES
+    {
+    "num_items_bought": "integer",
+    "money_paid": "integer"
+    }
+    """
 
     with db.engine.begin() as connection:
         # insert into processed
@@ -81,20 +101,20 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             VALUES
             (:job_id, 'item_sale') returning id;
             """),
-            [{
+            {
                 'job_id': cart_id
-        }]).fetchone()[0]
+        }).fetchone()[0]
 
         connection.execute(sqlalchemy.text("""                    
             INSERT INTO stock_ledger (trans_id, product_id, change, description)
             SELECT :trans_id, product_id, -1 * quantity, :description
             FROM cart_items
             WHERE cart_items.cart_id = :cart_id"""),
-            [{
+            {
                 'trans_id': trans_id,
                 'description': 'sale',
                 'cart_id': cart_id
-        }])
+        })
         
         connection.execute(sqlalchemy.text("""
             INSERT INTO money_ledger(trans_id, change, description)
@@ -102,23 +122,23 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             FROM cart_items
             WHERE cart_items.cart_id = :cart_id
             GROUP BY cart_items.cart_id;"""),
-            [{
+            {
                 'trans_id': trans_id,
                 'cart_id': cart_id,
                 'description': 'sale'
-        }])
+        })
 
         # get quantity of items sold
         quant_bought = connection.execute(sqlalchemy.text("SELECT SUM(change) as total FROM stock_ledger WHERE trans_id = :trans_id"),
-                                          [{
+                                          {
                                               'trans_id': trans_id
-                                          }]).fetchone().total * -1
+                                          }).fetchone().total * -1
 
         # get money paid
         income = connection.execute(sqlalchemy.text("SELECT change FROM money_ledger WHERE trans_id = :trans_id"),
-                                    [{
+                                    {
                                         'trans_id': trans_id
-                                    }]).fetchone().change
+                                    }).fetchone().change
         
     print("stock bought: " + str(quant_bought) + " money paid: " + str(income))
 
