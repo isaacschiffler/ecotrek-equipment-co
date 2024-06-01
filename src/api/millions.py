63d78673d6668d220d378c05c532120a31dfc8f9
Isaac Schiffler <ischiffl@calpoly.edu>
2024-05-31 17:25:20 -0700

    more progress

--- src/api/millions.py
+++ src/api/millions.py
@@ -31,6 +31,9 @@ def add_products():
     products = []
 
     with db.engine.begin() as connection:
+        # add lots of cash...
+        connection.execute(sqlalchemy.text("""INSERT INTO money_ledger (change, description)
+                                           VALUES (30000000, 'lots of money to simulate with...')"""))
         # clothing proudcts
         id = connection.execute(sqlalchemy.text("""SELECT id
                                                           FROM categories
@@ -132,7 +135,7 @@ def buy_stock():
             sku=i["sku"],
             category_id=i["cat_id"],
             price=round(i["sale_price"] * (1 / 1.2), 2),
-            quantity=20
+            quantity=4000
         ))
     stock.post_deliver_stock(buy_plan, 1)
     print("products bought and delivered...")
@@ -143,4 +146,64 @@ def make_users():
     # probably like 100,000 users
     # each user has 2 carts?
     # each cart as 3-4 items?
+
+    # initiate product fake data
+    buy_stock()
+
+    users = []
+
+    possible_activities = ["Hiking", "Camping", "Running", "Cooking", "Backpacking", "Hunting", "Fishing"]
+
+    for i in range(0, 100000):
+        name = fake.name()
+        email = fake.email()
+        number = random.randint(1000000000, 9999999999) # using this for phone number instead of fake.phone_number() b/c the database was set to int8 not text
+        activities = random.sample(possible_activities, 3)
+        users.append({
+            "name": name,
+            "email": email,
+            "number": number,
+            "activities": activities
+        })
+    print("100,000 users generated")
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""INSERT INTO users (name, email, phone_number, preferred_activities)
+                                           VALUES (:name, :email, :number, :activities)"""),
+                                           users)
+    print("100,000 users inserted")
+
+    #create carts for each user
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""INSERT INTO carts (user_id)
+                                           SELECT id
+                                           FROM users"""))
+        connection.execute(sqlalchemy.text("""INSERT INTO carts (user_id)
+                                           SELECT id
+                                           FROM users"""))
+    print("200,000 total carts created")
+
+    #add items to each cart
+    with db.engine.begin() as connection:
+        for j in range(0, 3):
+            for i in range(9, 74):
+                print("added product ", i, " to carts")
+                cart_id_high = ((i + j - 8) * 3333) % 200000
+                cart_id_low = ((i + j - 9) * 3333) % 200000
+                rand_quant = random.randint(1, 3)
+                connection.execute(sqlalchemy.text("""INSERT INTO cart_items (cart_id, product_id, quantity, price)
+                                                SELECT c.id as cart_id, p.id as product_id, :rand_num, p.sale_price
+                                                FROM carts as c
+                                                JOIN products as p on p.id = :prod_id
+                                                WHERE c.id > :low and c.id <= :high
+                                                """),
+                                                [{
+                                                    "rand_num": rand_quant,
+                                                    "prod_id": i,
+                                                    "low": cart_id_low,
+                                                    "high": cart_id_high
+                                                }])
+    print("over 630,000 items added to carts")
+
+    # now checkout for them all... definitely gonna be well over 1M, maybe just do 2 items per cart above?^^^^^
+
     return "OK"
