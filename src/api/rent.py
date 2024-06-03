54ce2e829e09c8e7e9b54e0964f9a7042b0847de
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2024-06-03 01:29:14 -0700

    error handling in rental request

--- src/api/rent.py
+++ src/api/rent.py
@@ -40,7 +40,28 @@ def rent_item(new_rental: NewRentalRequest):
     }
     """
     with db.engine.begin() as connection:
-        
+        # Check if customer exists
+        customer = connection.execute(text("""
+            SELECT id FROM users WHERE id = :customer_id
+        """), {"customer_id": new_rental.customer_id}).fetchone()
+
+        if customer is None:
+            return {"success": False, "message": "Customer does not exist"}
+
+        # Check if the product_id exists
+        product = connection.execute(text("""
+            SELECT id FROM products WHERE id = :product_id
+        """), {"product_id": new_rental.product_id}).fetchone()
+
+        if product is None:
+            return {"success": False, "message": "Product does not exist"}
+
+        # Validating end_time is after start_time
+        if new_rental.end_time <= new_rental.start_time:
+            return {"success": False, "message": "End time must be after start time"}
+
+
+        # Acceptable request
         connection.execute(sqlalchemy.text("""
             INSERT INTO rentals (customer_id, product_id, start_time, end_time)
             VALUES (:customer_id, :product_id, :start_time, :end_time)
@@ -50,7 +71,8 @@ def rent_item(new_rental: NewRentalRequest):
             'start_time': new_rental.start_time,
             'end_time': new_rental.end_time
         })
-    return {"message": "Rental request added successfully"}
+    
+    return {"success": True, "message": "Rental request added successfully"}
 
 @router.post("/return")
 def return_item(return_rental: ReturnRentalRequest):
